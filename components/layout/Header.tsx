bd7df27c72ea8542bf48c542fb46b466e576c300
Eloka Agu <eloka@satellitelabs.xyz>
2025-07-11 12:00:17 +0100

    UI: Restore original header nav padding (p-6 lg:px-8) for wider margin

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -164,7 +164,7 @@ export default function Header() {
           : "bg-transparent"
       )}
     >
-      <nav className="mx-auto flex w-full items-center justify-between p-4 lg:px-6">
+      <nav className="mx-auto flex w-full items-center justify-between p-6 lg:px-8">
         {/* Logo */}
         <div className="flex lg:flex-1">
           <NavigationLink href="/" className="-m-1.5 p-1.5">
